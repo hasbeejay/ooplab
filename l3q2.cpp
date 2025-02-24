651f6288b95ade7857a1181c2cafbcab4668bd59
Haseeb Jalil <hasbeejayleo@gmail.com>
2025-02-24 11:41:48 +0500

    Update l3q2.cpp

--- l3q2.cpp
+++ l3q2.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 class Car
@@ -9,23 +10,50 @@ private:
     int year;
 
 public:
-    void set_Brand(string brand)
+    void set_brand(string brand)
     {
         this->brand = brand;
     }
-    void set_Model(string model)
+    void set_model(string model)
     {
         this->model = model;
     }
-    void set_Year(int year)
+    void set_year(int year)
     {
         this->year = year;
     }
 
-    string get_Brand();
-    string get_Model();
-    int get_Year();
+    string get_brand()
+    {
+        return brand;
+    }
+    string get_model()
+    {
+        return model;
+    }
+    int get_year()
+    {
+        return year;
+    }
 };
 
+int main()
+{
+    Car car1;
+    car1.set_brand("Ford");
+    car1.set_model("Mustang");
+    car1.set_year(1969);
 
+    cout << car1.get_brand() << endl;
+    cout << car1.get_model() << endl;
+    cout << car1.get_year() << endl;
 
+    Car car2;
+    car2.set_brand("BMW");
+    car2.set_model("X5");
+    car2.set_year(1999);
+
+    cout << car2.get_brand() << endl;
+    cout << car2.get_model() << endl;
+    cout << car2.get_year() << endl;
+}
